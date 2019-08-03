b3407ea3ac0a1de110243b4cac8c528365050dfa
caixiaosong <895180002@qq.com>
2019-08-04 02:46:59 +0800

    关闭IORedis的Mocha测试

--- test/ioredis/T_Ioreids.ts
+++ test/ioredis/T_Ioreids.ts
@@ -2,19 +2,33 @@ import { assert } from 'chai';
 import * as RDS from 'ioredis';
 
 
-describe('ioredis', function () {
-    let redis: RDS.Redis;
+/**
+ * edis 测试的需要开启redis后才能进行测试，所以这里采用skip过滤掉mocha的测试
+ * */
+
+describe.skip('ioredis', function () {
+    let redis: RDS.Redis = null;
     before(function () {
         redis = new RDS(6379, '127.0.0.1');
     });
 
-    it('get null num', async function () {
+    after(function () {
+        if (redis !== null) {
+            redis.disconnect();
+        }
+        redis = null;
+    })
+
+
+    it('get null num', async function (done: Function) {
         try {
             const value = await redis.get('any_null_key');
             assert.equal(value, null);
         } catch (reson) {
-
+            this.skip();
         }
     });
 
 });
+
+
